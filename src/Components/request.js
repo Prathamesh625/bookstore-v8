7a2dcaf60478dd65a51d6a433e96e1315f3eb57b
Prathamesh Dipak Thorat <92034832+Prathamesh625@users.noreply.github.com>
2023-07-15 11:55:06 +0530

    Update request.js

--- src/Components/request.js
+++ src/Components/request.js
@@ -1,6 +1,6 @@
 const requests = {
-  mpsc: "https://bookapi-2.herokuapp.com/book/searchbook/mpsc",
-  upsc: "https://bookapi-2.herokuapp.com/book/searchbook/mpsc",
+  mpsc: "https://bookapi-weil.onrender.com/book/searchbook/mpsc",
+  upsc: "https://bookapi-weil.onrender.com/book/searchbook/mpsc",
 };
 
 export default requests;
